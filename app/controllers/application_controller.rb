f40831f844c5064ea3bfa3cf831c50e0f3aad0ed
adriangohjw <adriangohjw@gmail.com>
2021-12-01 22:47:02 +0800

    Add path for displaying record content as json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   def index
     @api_contents = ApiContent.order(created_at: :desc).first(100)
   end
+
+  def show
+    @api_content = ApiContent.find(params[:id])
+    render json: @api_content.content.to_json
+  end
 end
