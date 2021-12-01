38852d2f5784ef427e0ac8f4847c4bf21845030e
adriangohjw <adriangohjw@gmail.com>
2021-12-02 00:25:53 +0800

    Add ability to create ApiContent

---
+++ app/controllers/api_content_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'json'
+
+class ApiContentController < ActionController::Base
+  def create
+    api_content = ApiContent.new(
+      title: create_params[:title],
+      content: JSON.parse(create_params[:content]).to_json
+    )
+    api_content.save!
+  rescue StandardError => e
+    flash.notice = e.message
+  ensure
+    redirect_to root_path
+  end
+
+  private
+
+  def create_params
+    params.permit(:title, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#index'
   get '/', to: 'application#index'
+  post '/api_content/create', to: 'api_content#create'
   get '/all/public/:id', to: 'application#show'
 end
